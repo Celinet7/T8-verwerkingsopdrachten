1af859612d38e0edd89f509fa6cbdf41271765b2
Celinet7 <112889@emmauscollege.nl>
2023-09-26 10:30:31 +0000

    opdracht 3c

--- script.js
+++ script.js
@@ -77,6 +77,71 @@ class Mens {
 
 }
 
+class Kat {
+  x;
+  y;
+  speedX;
+  speedY;
+  breedte;
+  isBesmet;
+
+  constructor(newX, newY, newSpeedX, newSpeedY) {
+    this.x = newX;
+    this.y = newY;
+    this.speedX = newSpeedX;
+    this.speedY = newSpeedY;
+    this.isBesmet = false;
+    this.breedte = 10;
+
+  }
+
+  show() {
+    noStroke();
+
+    if (this.isBesmet === true) {
+      fill(255, 155, 0); //oranje
+    } else {
+      fill(0, 0, 255); //blauw
+    }
+
+    rect(this.x, this.y, this.breedte, this.breedte);
+  }
+
+  update() {
+    this.x = this.x - this.speedX;
+    this.y = this.y - this.speedY;
+
+    // stuiter evt. tegen de kanten
+    if (this.x <= 0 || this.x + this.breedte >= width) {
+      this.speedX = this.speedX * -1;
+    }
+    if (this.y <= 0 || this.y + this.breedte >= height) {
+      this.speedY = this.speedY * -1;
+    }
+  }
+
+  isOverlappend(andereKat) {
+    // zet teruggeefwaarde standaard op false
+    var overlappend = false;
+
+    // zet teruggeefwaarde op true als er een overlap is
+    if ((
+      this.x < andereKat.x + andereKat.breedte &&
+      this.x + this.breedte > andereKat.x &&
+      this.y < andereKat.y + andereKat.breedte &&
+      this.y + this.breedte > andereKat.y)
+
+    ) {
+
+      overlappend = true;
+    }
+
+    // stuur de teruggeefwaarde terug
+    return overlappend;
+  }
+
+}
+
 var actoren = [];
 
 
@@ -113,6 +178,23 @@ function setup() {
   }
 
   actoren[0].isBesmet = true;
+
+  // maak 10 katten
+  for (var teller = 0; teller < 10; teller++) {
+    var ruimteTotRand = 50;
+
+    // creëer random positie en snelheid
+    var randomX = random(ruimteTotRand, width - ruimteTotRand);
+    var randomY = random(ruimteTotRand, height - ruimteTotRand);
+    var randomSpeedX = random(-2, 2);
+    var randomSpeedY = random(-2, 2);
+
+    // maak nieuw mensobject
+    var nieuwKat = new Kat(randomX, randomY, randomSpeedX, randomSpeedY);
+
+    // voeg mensobject toe aan array
+    actoren.push(nieuwKat);
+  }
 }
 
 /**
